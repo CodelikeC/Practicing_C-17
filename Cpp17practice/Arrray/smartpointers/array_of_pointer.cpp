cd03dc746a449fa7b02b60a38c1ee0e5c04368dc
CodeLikeC <163371562+CodelikeC@users.noreply.github.com>
2025-01-30 17:02:31 +0700

    Update array_of_pointer.cpp

--- Cpp17practice/Arrray/smartpointers/array_of_pointer.cpp
+++ Cpp17practice/Arrray/smartpointers/array_of_pointer.cpp
@@ -1,1 +1,33 @@
+#include <iostream> 
+using namespace std; 
+#include <iomanip>
+#include <array> 
+int main ()
+{
+    cout <<"Array of the pointer" << endl; 
+    const char *pstars[]
+    {
+        "Fatty", "Clara Bow", 
+        "Lassie", "Slim Pickens", 
+        "Boris Karloff", "Mae West", 
+        "Oliver Hardy", "Greta Garbo"
+    };
 
+    cout <<"Pick a lucky star! Enter a number between 1 and" 
+    << sizeof(pstars) << endl;
+
+    size_t choice ; 
+    cin >> choice; 
+
+    if (choice >= 1 and choice <+ sizeof(pstars))
+    {
+        cout <<"Your lucky star is" << pstars[choice -1] << endl; 
+
+    }
+    else 
+    {
+        cout <<"Sorry, you have not got a lucky star" << endl; 
+    }
+    return 0 ;
+
+}
